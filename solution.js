713e039b8d106e0e2fa190633779dbfb865fa4f0
PaulZhuzliakov <paulparatrooper@gmail.com>
2021-11-21 11:17:08 +0300

    собирается первая строка

--- solution.js
+++ solution.js
@@ -1,56 +1,95 @@
 function solvePuzzle(pieces) {
 
-  let firstElemRightEdgeTypeId = getRightEdgeId(pieces[0].edges);
+    const ROW_SIZE = 10;
 
-  let nextElemId;
+    //ID ушка правой стороны первого пазла
+    let rightEdgeOfFirstElem = getFirstElemEdgeIds(pieces[0].edges)[0];
+    //ID ушка нижней стороны первого пазла
+    let bottomEdgeOfFirstElem = getFirstElemEdgeIds(pieces[0].edges)[1];
 
-  for (var i = 1; i < pieces.length; i++) {
+    let resultArr = [];
+    let firstElem = pieces[0].id;
+    resultArr.push(firstElem);
 
-    if (pieces[i].edges.top != null) {
-      if (firstElemRightEdgeTypeId == pieces[i].edges.top.edgeTypeId) {
-        nextElemId = pieces[i].id;
-        break;
-      }
-    }
-    if (pieces[i].edges.right != null) {
-      if (firstElemRightEdgeTypeId == pieces[i].edges.right.edgeTypeId) {
-        nextElemId = pieces[i].id;
-        break;
-      }
-    }
-    if (pieces[i].edges.bottom != null) {
-      if (firstElemRightEdgeTypeId == pieces[i].edges.bottom.edgeTypeId) {
-        nextElemId = pieces[i].id;
-        break;
-      }
-    }
-    if (pieces[i].edges.left != null) {
-      if (firstElemRightEdgeTypeId == pieces[i].edges.left.edgeTypeId) {
-        nextElemId = pieces[i].id;
-        break;
-      }
-    }
-  }
+    let i = 0;
+    rightEdge = rightEdgeOfFirstElem;
 
-  return [pieces[0].id, nextElemId];
+    do {
+        let temp = getElemOfRow(pieces, rightEdge);
+        let nextElem = temp[0];
+        let currElemEdge = temp[1];
+        rightEdge = currElemEdge;
+        resultArr.push(nextElem);
 
+        i++;
+    } while (i < ROW_SIZE-1);
+
+    return resultArr;
 }
 
-function getRightEdgeId(edges) {
-  let rightEdgeId;
-  if (edges.top != null && edges.right != null) {
-    rightEdgeId = edges.top.edgeTypeId;
-  }
-  if (edges.right != null && edges.bottom != null) {
-    rightEdgeId = edges.right.edgeTypeId;
-  }
-  if (edges.bottom != null && edges.left != null) {
-    rightEdgeId = edges.bottom.edgeTypeId;
-  }
-  if (edges.left != null && edges.top != null) {
-    rightEdgeId = edges.left.edgeTypeId;
-  }
-  return rightEdgeId;
+//возвращает ID ушка правой и нижней стороны первого пазла
+function getFirstElemEdgeIds(edges) {
+    let rightEdge;
+    let bottomEdge;
+    if (edges.top != null && edges.right != null) {
+        rightEdge = edges.top;
+        bottomEdge = edges.right;
+    }
+    if (edges.right != null && edges.bottom != null) {
+        rightEdge = edges.right;
+        bottomEdge = edges.bottom;
+    }
+    if (edges.bottom != null && edges.left != null) {
+        rightEdge = edges.bottom;
+        bottomEdge = edges.left;
+    }
+    if (edges.left != null && edges.top != null) {
+        rightEdge = edges.left;
+        bottomEdge = edges.top;
+    }
+    return [rightEdge, bottomEdge];
+}
+
+function getElemOfRow(pieces, rightEdgeOfPrevElem) {
+
+    let elemId;
+    let rightEdgeOfCurrElem;
+    for (let i = 0; i < pieces.length; i++) {
+        if (pieces[i].edges.top != null) {
+            if (rightEdgeOfPrevElem.edgeTypeId === pieces[i].edges.top.edgeTypeId
+                && rightEdgeOfPrevElem.type != pieces[i].edges.top.type) {
+                elemId = pieces[i].id;
+                rightEdgeOfCurrElem = pieces[i].edges.bottom;
+                break;
+            }
+        }
+        if (pieces[i].edges.right != null) {
+            if (rightEdgeOfPrevElem.edgeTypeId === pieces[i].edges.right.edgeTypeId
+                && rightEdgeOfPrevElem.type != pieces[i].edges.right.type) {
+                elemId = pieces[i].id;
+                rightEdgeOfCurrElem = pieces[i].edges.left;
+                break;
+            }
+        }
+        if (pieces[i].edges.bottom != null) {
+            if (rightEdgeOfPrevElem.edgeTypeId === pieces[i].edges.bottom.edgeTypeId
+                && rightEdgeOfPrevElem.type != pieces[i].edges.bottom.type) {
+                elemId = pieces[i].id;
+                rightEdgeOfCurrElem = pieces[i].edges.top;
+                break;
+            }
+        }
+        if (pieces[i].edges.left != null) {
+            if (rightEdgeOfPrevElem.edgeTypeId === pieces[i].edges.left.edgeTypeId
+                && rightEdgeOfPrevElem.type != pieces[i].edges.left.type) {
+                elemId = pieces[i].id;
+                rightEdgeOfCurrElem = pieces[i].edges.right;
+                break;
+            }
+        }
+
+    }
+    return [elemId, rightEdgeOfCurrElem];
 }
 
 // Не удаляйте эту строку
